4bb9330497e8defe622bcf5907724bf1aafffad1
pablokadafi <pablokadafi@hotmail.com>
2023-11-13 17:21:45 -0300

    projeto site pet
    projetinho

---
+++ Aula2-pawshop/js/script.js
@@ -0,0 +1,22 @@
+
+
+//seletor de elemntos
+const botaoTema = document.querySelector("#botaoTema")
+
+
+//adiciona um  evento
+botaoTema.addEventListener("click", mudaTema)
+
+function mudaTema(){
+    let body = document.querySelector("body")
+    let temaAtual = document.querySelector("#temaAtual")
+
+    if(botaoTema.checked){
+        body.setAttribute('data-bs-theme','dark')
+        temaAtual.innerHTML = "Escuro"
+    }else{
+        body.setAttribute('data-bs-theme', 'light')
+        temaAtual.innerHTML = "claro"
+    }
+    
+}
